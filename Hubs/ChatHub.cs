88993e8a817d63ded825926aec5ad873a01a30ee
Ramyakolanchinathan <123582867+Ramyakolanchinathan@users.noreply.github.com>
2024-04-01 12:32:01 +0530

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHubs.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string connectionId, string user,  string message)
+        {
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage",user,message);
+        }
+    }
+}
---
+++ js/Chat.js
@@ -0,0 +1,29 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+debugger;
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+   
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+
+    var conn = connection.connectionId;
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+   // var message = document.getElementById("messageInput").value;
+    
+    connection.send("SendMessage", conn, user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
